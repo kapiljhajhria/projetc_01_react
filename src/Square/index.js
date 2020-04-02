9fa476df9673922ef6b8bd101a5d3a10fbb495aa
kapil jhajhria <jhajhria44@gmail.com>
2020-04-02 11:17:43 +0530

    day 110 task 1 completed
    change color from red to green
    chnage value from o to X on clicking button

--- src/Square/index.js
+++ src/Square/index.js
@@ -2,10 +2,29 @@ import React from 'react'
 import './styles.css'
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value:'0',
+            color: 'red'
+        }
+    }
+
+
     render() {
         return (
-            <button className="button-square">
-                {this.props.indexNumber}
+            <button className="button-square" onClick={() =>
+                this.setState({
+                    value: 'X',
+                    color:'green'
+                })}
+                    style={{color:this.state.color}}
+                    hover={() =>
+                this.setState({
+                    value:'1'
+                })
+                }>
+                {this.state.value}
             </button>
         );
     }
