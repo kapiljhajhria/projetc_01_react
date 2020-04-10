42f81e156be2a8dc74c04adfeef3e5198d9fa900
kapil jhajhria <jhajhria44@gmail.com>
2020-04-10 20:29:18 +0530

    prep for using function instead of Class for Game Component

--- src/Game/index.js
+++ src/Game/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './styles.css'
 import Grid from "../Grid";
 
@@ -156,12 +156,12 @@ class Game extends React.Component {
         let newHistory = this.state.history.slice(0, moves + 2);
 
 
-        this.state.gameArray=newGameArray;
-        this.state.isXnext= moves % 2 === 0;
-        this.state.status= newStatus;
-        this.state.history= newHistory;
-        this.state.gameOver= false;
-        this.state.isGameDraw= false;
+        this.state.gameArray = newGameArray;
+        this.state.isXnext = moves % 2 === 0;
+        this.state.status = newStatus;
+        this.state.history = newHistory;
+        this.state.gameOver = false;
+        this.state.isGameDraw = false;
     }
 
     addHistoryBtn(gameArray) {
@@ -221,6 +221,200 @@ class Game extends React.Component {
 
         );
     }
+}
+
+function GameF() {
+    const [gameArray, setGameArray] = useState([Array(9).fill(null)]);
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [isXnext,setIsNext] = useState(true);
+    const [status,setStatus] = useState('Player X turn');
+    const [gameOver,setGameOver] = useState(false);
+    const [isGameDraw,setIsGameDraw]  = useState(false);
+    const [history,setHistory] = useState([<div>Game History</div>]);
+
+    const onHistoryBtnPress=(index)=>{
+        console.log(gameArray.length);
+        console.log(gameArray);
+        let temp = gameArray[index];
+        console.log('temp is :'+temp);
+        setSquares(temp);
+    }
+
+    const handleClick = (index) => {
+        let squaresCopy = [].concat(squares);
+        if (squaresCopy[index] !== null) {
+            return;
+        }
+        if (squares.filter((x) => x !== null).length !== gameArray.length - 1) {
+            continueGameFromCurrentState(squaresCopy);
+
+        }
+        if (gameOver) {
+            if (isGameDraw) {
+                alert("No More Moves left, Please start over");
+            } else {
+                alert("game Won, refresh page to start over");
+            }
+
+        } else {
+            let squaresCopy = [].concat(squares);
+            if (squaresCopy[index] !== null) {
+                return;
+            }
+            if (squares.filter((x) => x !== null).length !== gameArray.length - 1) {
+                continueGameFromCurrentState(squaresCopy);
+
+            }
+            squaresCopy[index] = isXnext ? 'X' : 'O';
+            let tempGameArray = gameArray.slice();
+            tempGameArray.push(squaresCopy);
+            console.log('this tempArray will be pushed to addHistoryBtn andits length is '+tempGameArray.length);
+            addHistoryBtn(tempGameArray);
+            // console.log('tempGameArray is :'+tempGameArray);
+            if (gameWon(squaresCopy)) {
+                setGameArray(tempGameArray);
+                setSquares(squaresCopy);
+                setStatus(!isXnext ? 'Player O Won' : 'Player X Won');
+                setGameOver(true);
+            } else if (gameDraw(squaresCopy)) {
+                setGameArray(tempGameArray);
+                setSquares(squaresCopy);
+                setStatus('Game Drawn');
+                setGameOver(true);
+                setIsGameDraw(true);
+            } else {
+                setGameArray(tempGameArray);
+                setSquares(squaresCopy);
+                setIsNext(!isXnext);
+                setStatus(!isXnext ? 'Player O Won' : 'Player X Won');
+
+            }
+        }
+        //update the state
+
+    }
+
+    const gameDraw=(squaresList)=>{
+        return squaresList.every((el) => el !== null);
+    }
+
+    const gameWon=(squaresList)=>{
+        if (squaresList[0] === 'X' && squaresList[1] === 'X' && squaresList[2] === 'X')
+            return true;
+        else if (squaresList[0] === 'O' && squaresList[1] === 'O' && squaresList[2] === 'O')
+            return true;
+        else if (squaresList[3] === 'X' && squaresList[4] === 'X' && squaresList[5] === 'X')
+            return true;
+        else if (squaresList[3] === 'O' && squaresList[4] === 'O' && squaresList[5] === 'O')
+            return true;
+        else if (squaresList[6] === 'X' && squaresList[7] === 'X' && squaresList[8] === 'X')
+            return true;
+        else if (squaresList[6] === 'O' && squaresList[7] === 'O' && squaresList[8] === 'O')
+            return true;
+        else if (squaresList[0] === 'X' && squaresList[3] === 'X' && squaresList[6] === 'X')
+            return true;
+        else if (squaresList[0] === 'O' && squaresList[3] === 'O' && squaresList[6] === 'O')
+            return true;
+        else if (squaresList[1] === 'X' && squaresList[4] === 'X' && squaresList[7] === 'X')
+            return true;
+        else if (squaresList[1] === 'O' && squaresList[4] === 'O' && squaresList[7] === 'O')
+            return true;
+        else if (squaresList[2] === 'X' && squaresList[5] === 'X' && squaresList[8] === 'X')
+            return true;
+        else if (squaresList[2] === 'O' && squaresList[5] === 'O' && squaresList[8] === 'O')
+            return true;
+        else if (squaresList[0] === 'X' && squaresList[4] === 'X' && squaresList[8] === 'X')
+            return true;
+        else if (squaresList[0] === 'O' && squaresList[4] === 'O' && squaresList[8] === 'O')
+            return true;
+        else if (squaresList[2] === 'X' && squaresList[4] === 'X' && squaresList[6] === 'X')
+            return true;
+        else if (squaresList[2] === 'O' && squaresList[4] === 'O' && squaresList[6] === 'O')
+            return true;
+        return false;
+    }
+
+    const gameReset=()=>{
+        setGameArray([Array(9).fill(null)]);
+        setSquares(Array(9).fill(null));
+        setIsNext(true)
+        setStatus('Player X turn');
+        setGameOver(false);
+        setIsGameDraw(false);
+        setHistory([<div>Game History</div>]);
+    }
+
+    const continueGameFromCurrentState=(squaresCopy) =>{
+        let moves = squaresCopy.filter((x) => x != null).length;
+        let newGameArray = gameArray.slice(0, moves + 1);
+        let newStatus = moves % 2 === 0 ? 'Player X turn' : 'Player O turn';
+        let newHistory = history.slice(0, moves + 2);
+
+        setGameArray(newGameArray);
+        setIsNext(moves % 2 === 0)
+        setStatus(newStatus);
+        setGameOver(false);
+        setIsGameDraw(false);
+        setHistory(newHistory);
+        // gameArray = newGameArray;
+        // isXnext = moves % 2 === 0;
+        // status = newStatus;
+        // history = newHistory;
+        // gameOver = false;
+        // isGameDraw = false;
+    }
+    const addHistoryBtn=(gameArray)=> {
+        let gameArrayCopy = [].concat(gameArray);
+        console.log('history length so far is :'+history.length);
+        let historyCopy = [].concat(history);
+        let moves = gameArray.length-1;
+        console.log('moves are:'+moves);
+        if (moves === 1) {
+            historyCopy.push(
+                <div className={"historybtn"}>
+                    <div>
+                        {moves - 1}.
+                        <button onClick={() => {
+                            gameReset()
+                        }}>
+                            Restart Game
+                        </button>
+                    </div>
+                </div>
+            );
+        }
+        historyCopy.push(
+            <div className={"historybtn"}>
+                <div>
+                    {moves}.
+                    <button onClick={() => {
+                        onHistoryBtnPress(moves )
+                    }}>
+                        Go back to move no {moves}
+                    </button>
+                </div>
+            </div>
+        );
+        setHistory(historyCopy);
+    }
+
+
+
+    return (
+        <div className="gameboard">
+            <div className="left">
+                <div className={"gamestatus"}>{status}</div>
+                <div>
+                    <Grid squares={squares} handleClick={(index) => handleClick(index)}/>
+                </div>
+            </div>
+            <div className={"right"}>
+                {history}
+            </div>
+        </div>
+
+    );
+
 }
 
 export default Game;
